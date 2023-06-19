0265e88a082f431b8c4d4f7731094a6fb17d8e0c
ipsit33 <papani.mohanty@gmail.com>
2023-06-19 09:52:00 +0530

    second commit

---
+++ client/dictionaryapp/src/component/home.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import rj from "./song.mp3";
+import { GiSpeaker } from 'react-icons/gi';
+
+function HomePage() {
+  const audio = new Audio(rj);
+
+  const handleAudio = () =>{
+    audio.play();
+  };
+
+  return (
+    <div className="homepage">
+      <h1>Word Of the Day</h1>
+      <div className="box">
+        <h1>React JS</h1>
+        <button onClick={handleAudio}><GiSpeaker /></button>
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
