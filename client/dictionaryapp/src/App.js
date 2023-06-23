2705f268922da178f0d39536d4dcda0d3a7eb4f0
sekhar suman sahoo <sekharsumansaho@gmail.com>
2023-06-23 11:11:00 +0530

    first commit

---
+++ client/dictionaryapp/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Login from './pages/signin/signin';
+
+const App = () => {
+  return (
+    <div>
+      
+      <Login/>
+    </div>
+  );
+};
+
+export default App;
