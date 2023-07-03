b901cd6bc816cf6e8fc184e2f868ba60484e2317
ipsit33 <85749577+ipsit33@users.noreply.github.com>
2023-07-03 20:39:01 +0530

    Update UserSchema.js

--- server/model/UserSchema.js
+++ server/model/UserSchema.js
@@ -72,7 +72,7 @@ userSchema.pre('save', async function(next){
 // generating auth token
 userSchema.methods.generateAuthToken = async function(){
     try{
-        let token = jwt.sign({_id : this._id}, "secret");
+        let token = jwt.sign({_id : this._id}, process.env.SECRET_KEY);
         this.tokens = this.tokens.concat({token: token});
         await this.save();
         return token;
