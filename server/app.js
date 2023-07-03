d2999d0dc01bcf2c507279f3f1df8e2b4bde661e
ipsit33 <85749577+ipsit33@users.noreply.github.com>
2023-07-03 20:22:18 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -3,18 +3,27 @@ const dotenv = require('dotenv');
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const app = express();
+const PORT = process.env.PORT || 8000;
 
 dotenv.config({path: './.env'});
-require('./db/conn');
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.DATABASE);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
 // const User = require('./model/UserSchema.js');'
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(require('./router/auth'));
 
-const PORT = process.env.PORT;
 
-
-app.listen(PORT,() => {
-    console.log(`Server is running at ${PORT} port`);
-});
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("listening for requests");
+    })
+})
