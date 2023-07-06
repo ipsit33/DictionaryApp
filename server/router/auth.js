2bc08a32ab453fff635094e8ad0304fe6b50d6ac
ipsit33 <85749577+ipsit33@users.noreply.github.com>
2023-07-06 11:05:17 +0530

    Update auth.js

--- server/router/auth.js
+++ server/router/auth.js
@@ -62,7 +62,8 @@ router.post('/login',async (req,res) => {
 
             res.cookie("jwtoken",token,{
                 expires: new Date(Date.now() + 25892000000),
-                httpOnly: true
+                httpOnly: true,
+                sameSite: 'lax'
             });
             
 
