30c0967a7405a0cd7492c1332c7c22bb25117823
ipsit33 <85749577+ipsit33@users.noreply.github.com>
2023-07-06 10:19:27 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const app = express();
+const cors = require('cors');
 const PORT = process.env.PORT;
 
 dotenv.config({path: './.env'});
@@ -18,6 +19,7 @@ const connectDB = async () => {
 // const User = require('./model/UserSchema.js');'
 
 app.use(express.json());
+app.use(cors());
 app.use(cookieParser());
 app.use(require('./router/auth'));
 
