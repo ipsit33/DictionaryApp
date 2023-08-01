13ed345f995d5c95c7665157737b2a73f96a4caa
Ipsit Spandan Mohanty <85749577+ipsit33@users.noreply.github.com>
2023-08-01 18:47:47 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -20,7 +20,7 @@ const connectDB = async () => {
 // const User = require('./model/UserSchema.js');'
 
 
-app.use(cors());
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
 app.use(express.static(path.join(__dirname, 'build')));
 app.use(express.json());
 app.use(cookieParser());
